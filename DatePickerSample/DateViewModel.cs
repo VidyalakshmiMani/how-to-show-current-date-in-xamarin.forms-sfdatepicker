c93a0f766f3e2fcc78e969ce7fa4bd52f7d89812
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-07-12 18:12:51 +0530

    DatePicker sample has been added

---
+++ DatePickerSample/DateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DatePickerSample
+{
+    public class DateViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private DateTime selectedDate = DateTime.Now;
+
+        /// <summary>
+        /// Gets or sets the selected date of SfDatePicker
+        /// </summary>
+        public DateTime SelectedDate
+        {
+            get { return selectedDate; }
+            set
+            {
+                selectedDate = value;
+                NotifyPropertyChanged("SelectedDate");
+            }
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
